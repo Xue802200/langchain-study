680a251bd949eb28ac6c01363011450d5f4fb922
xue <2518880465@qq.com>
2026-04-08 08:11:31 +0800

    各种不同的消息列表

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import langchain
+from langchain_core.prompts import ChatPromptTemplate
 
-if __name__ == '__main__':
-    print(langchain.__version__)
+template = ChatPromptTemplate.from_messages([
+    ("system","你是世界级的技术文档编写者"),
+    ("user","{input}")
+])
+
+if __name__ == "__main__":
+    print(template)
